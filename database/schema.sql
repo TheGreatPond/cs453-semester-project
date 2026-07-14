05fb059f16e778a3caa7d1e5d223437f9b6b8f2e
Lake <mls0068@uah.edu>
2026-07-13 22:02:27 -0500

    added starting data to schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -6,3 +6,6 @@ CREATE TABLE IF NOT EXISTS tasks (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+INSERT INTO TASKS (id, title, description, status, created_at, updated_at) VALUES ('3', 'third task', 'this is the third task.', 'not started', '2026-07-14 02:50:21.716814+00', '2026-07-14 02:50:21.716814+00') RETURNING id, title, description, status, created_at, updated_at;
+INSERT INTO TASKS (id, title, description, status, created_at, updated_at) VALUES ('2', 'second task', 'this is the second task.', 'done', '2026-07-14 02:47:21.716814+00', '2026-07-14 02:47:21.716814+00') RETURNING id, title, description, status, created_at, updated_at;
+INSERT INTO TASKS (id, title, description, status, created_at, updated_at) VALUES ('1', 'first_task', 'this is the first task.', 'in progress', '2026-07-14 02:46:21.716814+00', '2026-07-14 02:46:21.716814+00') RETURNING id, title, description, status, created_at, updated_at;
